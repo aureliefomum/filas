ec5a75909ea408af48b0a75d2cfdbd91d925fde3
aureliefomum <azam_aurelie@yahoo.com>
2022-03-04 19:07:12 -0300

    Refatorando a classe No para utilizar wildcard

--- src/main/java/one/digitalinnovation/No.java
+++ src/main/java/one/digitalinnovation/No.java
@@ -1,13 +1,13 @@
 package one.digitalinnovation;
 
-public class No {
-    private Object object;
-    private No refNo;
+public class No<T> {
+    private T object;
+    private No<T> refNo;
 
     public No(){
 
            }
-    public No(Object object){
+    public No(T object){
         this.refNo = null;
         this.object = object;
     }
@@ -23,7 +23,7 @@ public class No {
                 '}';
     }
 
-    public void setObject(Object object) {
+    public void setObject(T object) {
         this.object = object;
     }
 
